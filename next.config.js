3d59dbd3982df80135e0cca16be10deaa2cb6290
swapna-uggam <149858443+swapna-uggam@users.noreply.github.com>
2025-07-12 13:08:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-  remotePatterns: [
-    { protocol: 'https', hostname: 'i.ytimg.com' },
-    { protocol: 'https', hostname: 'i.pinimg.com' },
- 
-  ],
-   },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.southindiafashion.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'southjewellery.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com', // ✅ Add this for YouTube thumbnails
+      },
+    ],
+  },
 };
-
 
 module.exports = nextConfig;
