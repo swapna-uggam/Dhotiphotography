e54fd0f3e233c934577a9fc7cb6d0599508c424c
swapna-uggam <149858443+swapna-uggam@users.noreply.github.com>
2025-07-12 12:32:55 +0530

    Update config.js

--- app/admin/Firebase/config.js
+++ app/admin/Firebase/config.js
@@ -1,14 +1,35 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD-OFu4Auc76mHTIFAoxtOSqVDlkMkygBM",
+//   authDomain: "snapu-dhoti.firebaseapp.com",
+//   projectId: "snapu-dhoti",
+//   storageBucket: "snapu-dhoti.firebasestorage.app",
+//   messagingSenderId: "236324759880",
+//   appId: "1:236324759880:web:efd49b634f21e0c6622b7d",
+//   measurementId: "G-6CRCQ7NW53"
+// };
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+
+
+// ==========================---------------------------
+
+
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyD-OFu4Auc76mHTIFAoxtOSqVDlkMkygBM",
-  authDomain: "snapu-dhoti.firebaseapp.com",
-  projectId: "snapu-dhoti",
-  storageBucket: "snapu-dhoti.firebasestorage.app",
-  messagingSenderId: "236324759880",
-  appId: "1:236324759880:web:efd49b634f21e0c6622b7d",
-  measurementId: "G-6CRCQ7NW53"
+  apiKey: "AIzaSyDpk4s32pH90Ouumh7LvWbIAXj-hdsyFTc",
+  authDomain: "dhoti-533dc.firebaseapp.com",
+  projectId: "dhoti-533dc",
+  storageBucket: "dhoti-533dc.firebasestorage.app",
+  messagingSenderId: "348498427743",
+  appId: "1:348498427743:web:e0e67ab3d018eb2bbe40ea",
+  measurementId: "G-GSN1FFYWL4"
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
